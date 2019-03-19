db0e48c3920971f12429f4bb202bd2f320d31ec6
Rupyes <rupyesmhj+github@gmail.com>
2019-03-19 14:51:38 +0530

    added models to admin

--- thinktank/softwares/admin.py
+++ thinktank/softwares/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import (Software, Configuration, WorkingProject, Technology,)
 
 # Register your models here.
+admin.site.register(Software)
+admin.site.register(Configuration)
+admin.site.register(WorkingProject)
+admin.site.register(Technology)
